cb81353c5e1e8cab781a5165b24ec7d461982f16
David R_ <36084356+infodavide@users.noreply.github.com>
2024-04-01 10:36:42 +0200

    Fixes on setup to format description as plain text (#7)
    * Dev ongoing, removal of variables to collect when stopping context.
    
    * Pylint configuration
    
    * Code review
    
    * Code review
    
    * Configuration for deployment
    
    * Fix on setup to be able to set a long description as plain text (no md)

--- setup.py
+++ setup.py
@@ -3,18 +3,21 @@
 """
 Setup
 """
-
+import markdown
 from setuptools import setup, find_packages
+from bs4 import BeautifulSoup
 
 with open('README.md', encoding='utf-8') as f:
-    readme_content = f.read()
+    html = markdown.markdown(f.read())
+    soup = BeautifulSoup(html, features='html.parser')
+    readme_content = soup.get_text()
 
 with open('LICENSE', encoding='utf-8') as f:
     license_content = f.read()
 
 setup(
     name='variables_collector', 
-    version='1.0.0', 
+    version='1.0.1',
     description='Variables collector', 
     long_description=readme_content,
     url='https://github.com/infodavide/data_collector',
